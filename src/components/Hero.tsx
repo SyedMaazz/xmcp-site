4189009252cf6227c14a107d06cbfe9beedd5d76
syedmaaz <maazdeys@gmail.com>
2026-02-17 23:33:27 +0530

    feat: headings in hero section done

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -9,12 +9,12 @@ export default function Hero() {
           <div className="flex items-center gap-2 text-[12px] text-white">
             <span>v0.6.2</span>
 
-            <span className="uppercase text-[9.8px] tracking-wide bg-[#171717] border border-dashed border-neutral-700 px-2 py-1 text-[#a8a8a8]">
+            <span className="uppercase text-[9.8px] bg-[#171717] border border-dashed border-neutral-700 px-2 py-1 text-[#a8a8a8]">
               Latest Version
             </span>
           </div>
           {/* HEADING */}
-          <h1 className="mt-6 w-[900px] h-[160px] text-[40px] font-normal tracking-tight text-neutral-200 leading-[64px] text-center">
+          <h1 className="mt-4.5 w-[900px] h-[160px] text-[40px] font-[350] tracking-normal text-neutral-200 leading-[46px] text-center">
             The TypeScript framework for
             <br />
             building & shipping MCP servers
